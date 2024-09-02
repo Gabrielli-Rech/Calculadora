cb2216df78fb659b9ff6b278abac80ddc6a58d2c
Gabrielli Rech dos Reis <182310018@senacinformatica.br>
2024-09-02 09:41:13 -0300

    first commit

---
+++ AppJava1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appjava1;
+
+/**
+ *
+ * @author 182310085
+ */
+public class AppJava1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int valor;
+        double ponto;
+        byte a;
+        boolean VouF;
+        
+        valor = 10;
+        
+        System.out.printf("valor de 'valor': " + valor + "\n");
+    }
+    
+}
